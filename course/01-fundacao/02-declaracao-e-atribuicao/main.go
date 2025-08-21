7ba14755392adafb8827d5a516dcd9ccb58c2ef6
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2025-08-21 19:14:00 -0300

    ✅3-Fundação / 2-Declaração e atribuição

---
+++ course/01-fundacao/02-declaracao-e-atribuicao/main.go
@@ -0,0 +1,20 @@
+package main
+
+const a = "Hello, World"
+
+var b bool
+
+var (
+	c int     = 10
+	d string  = "Antonio"
+	e float64 = 1.76
+)
+
+func main() {
+	a := "@"
+
+	println(a)
+}
+
+func x() {
+}
