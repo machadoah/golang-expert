23ab0df7f476607000252c9c472ad3ced7c83f06
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2025-08-21 21:12:36 -0300

    ✅ 3-Fundação / 10-Closures

---
+++ course/01-fundacao/10-closures/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	total := func() int {
+		return sum(1, 2, 3, 4, 5, 6, 7, 8, 9) * 2
+	}()
+
+	fmt.Println(total)
+}
+
+func sum(numbers ...int) int {
+	total := 0
+
+	for _, number := range numbers {
+		total = number + total
+	}
+
+	return total
+}
